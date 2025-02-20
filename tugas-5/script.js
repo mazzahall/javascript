edd9a4070a38d4f911a3795cf69bfef8cca22e56
mazzahall <abdullah0210@student.abudzar.sch.id>
2025-02-20 13:07:26 +0700

    tugas-5

---
+++ tugas-5/script.js
@@ -0,0 +1,153 @@
+let jumlahLayangan=5;
+let layangan1="merah";
+let layangan2 ="biru";
+let layangan3 ="hijau";
+let layangan4 = "kuning";
+let  layangan5="ungu";
+
+
+let layanganMasukGot =layangan3;
+let layanganDicuri =layangan5;
+
+//jawab 1
+console.log(`Jumlah layangan fuad sebelum rusak ${jumlahLayangan}`);
+
+//jawab 2
+console.log(`Layangan yang rusak yaitu ${layanganMasukGot} ${layanganDicuri}`);
+
+//jawab 3
+let total= jumlahLayangan - 2
+console.log(`Jumlah/total layangannya fuad setelah rusak adalah ${total}`);
+
+//latihan 2
+
+let jumlahIcecream = 3;
+let icecream1="Matcha";
+let icecream2="Chocolate";
+let icecream3="Vanilla";
+
+let memberikanIceCream=icecream2;
+
+//jawab 1
+console.log(`Jumlah ice cream Raffa adalah ${jumlahIcecream}`);
+
+//jawab 2
+console.log(`Rasa ice cream Raffa yang ke-2 adalah ${icecream2}`);
+
+
+//jawab 3
+console.log(`Rasa ice cream Raffa yang ke-3 adalah ${icecream3}`);
+
+//jawab 4
+console.log(`Ice cream Anhar adalah rasa ${memberikanIceCream}`);
+
+//jawab 5
+let Total= jumlahIcecream - 1
+console.log(`Total update ice cream Raffa setelah diberikan adalah ${Total}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Tugas form
+
+//soal 1
+let jumlahPermen=30;
+let memberikanPermen=10;
+let totalPermen=jumlahPermen - memberikanPermen;
+let duakalilipat=totalPermen*2;
+let kuadrat=duakalilipat*duakalilipat;
+ console.log(`Jumlah permen ana yang dimiliki Sekarang adalah ${duakalilipat}`);
+ console.log(`Jumlah permen ana adalah ${duakalilipat}, setelah diquadratkan adalah ${kuadrat}`);
+ 
+ //soal 2
+let jumlahKucing=2;
+let Kucing1="ali";
+let Kucing2="hasan";
+let KucingMix=Kucing1+Kucing2;
+console.log(`nama kucing pertama adalah ${Kucing1} dan nama kucing ke 2 adalah ${Kucing2}`);
+console.log(`penggabungan ke kucing pertama dan kucing ke dua adalah ${KucingMix}`);
+
+//soal 3
+let belalang = '12';
+let nyamuk = '11';
+let angsa = 10;
+let hasilBelalangNyamuk = +belalang + nyamuk;
+console.log(hasilBelalangNyamuk);
+let hasilNyamukAngsa = +nyamuk + angsa;
+console.log(hasilNyamukAngsa);
+console.log(`hasil pertama adalah ${hasilBelalangNyamuk}`);
+console.log(`hasil yang ke-2 adalah ${hasilNyamukAngsa}`);
+console.log(`penjelasan hasil pertama adalah karena string sama string`);
+console.log(`penjelasan hasil pertama adalah karena jumlahnya di konveksi`);
+
+//soal 4
+let bukufiksi=5;
+let bukunonfiksi=3;
+let pena=2;
+let hargafiksi=5000*bukufiksi;
+let harganonfiksi=7000*bukunonfiksi;
+let hargapena=2000*pena;
+let hargatotal=hargafiksi+harganonfiksi+hargapena;
+console.log(`Total semua biaya yang harus dibayar Aisha adalah ${hargatotal}`);
+
+//soal 5
+let penjumlahan1 = 5 + 3 * 2 ;
+let penjumlahan2 = (5 + 3) * 2 ;
+console.log(`Nilai penjumlahan yang pertama adalah ${penjumlahan1}`);
+console.log(`Nilai penjumlahan yang ke-2 adalah ${penjumlahan2}`);
+console.log(`Penjelasan penjumlahan yang pertama adalah karena jika ada perkalian maka kerjakan perkalian dahulu baru di tambah`);
+console.log(`Penjelasan penjumlahan yang pertama karena jika ada tanda kurung kerjakan yang ada di dalam kurung dulu baru dikalikan`);
+
+//soal 6
+let kresek = 4;
+let kotak = 6;
+let ember = 5 + (kresek = kotak);
+console.log(`Nilai isi kresek adalah ${kresek}`);
+console.log(`Nilai isi kotak adalah ${kotak}`);
+console.log(`Nilai isi ember adalah ${ember}`);
+console.log(`Penjelasan nilai dari ember adalah jika variabel kotak di ganti maka variabel kresek akan ikut keganti`);
+
+//tugas 7
+let x, y, z;
+z = 5+10+15 ;
+x=y
+y=z
+console.log(`nilai x adalah ${x}, nilai y adalah ${y}, dan nilai z adalah ${z}`);
+
+//tugas 8
+let money = 4;
+money = money * 2;
+money = money * 2;
+console.log(`nilai variabel money adalah ${money}`);
+
+// latihan 9 
+let J = 5;
+J++;
+let K = 8;
+K--;
+console.log(`Hasil variabel J adalah ${J} dan variable K adalah ${K}`);
+let penjelasanJ = `ia menggunakan increment yang membuat variabel J bertambah 1.`;
+let penjelasanK = `ia menggunakan decrement yang membuat variabel K berkurang 1.`;
+console.log(
+  `Hasil J demikian karena ${penjelasanJ} sedangkan variable K karena ${penjelasanK}`
+);
+
+// latihan 10 
+let room = "hafs";
+let age = 18;
+let shoesNumber = "42";
+let married = false;
+
+console.log(`${typeof room}`);
+console.log(`${typeof age}`);
+console.log(`${typeof shoesNumber}`);
+console.log(`${typeof married}`);
