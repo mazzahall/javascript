0ea35eac662967e48b05c7afdf19d314723b79b0
mazzahall <abdullah0210@student.abudzar.sch.id>
2025-02-21 09:05:11 +0700

    tugas-6

---
+++ tugas-6/script.js
@@ -0,0 +1,11 @@
+// latihan 1
+let dressSize = 45;
+dressSize = String(dressSize);
+console.log(dressSize);
+dressSize = +dressSize;
+console.log(dressSize);
+
+// latihan 2
+let isLight = true;
+isLight = String(isLight);
+console.log(isLight);
