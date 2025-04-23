ba94bd929af34f16f5cbc9ba55a77823cf86484e
mazzahall <abdullah0210@student.abudzar.sch.id>
2025-04-23 12:01:41 +0700

    tugas-9

---
+++ tugas-9/script.js
@@ -0,0 +1,26 @@
+// soal 1
+let nilaiMTK = 91;
+let nilaiIPA = 81;
+let nilaiIPS = 91;
+if (nilaiMTK >= 91 && nilaiIPA >= 91 && nilaiIPS >= 91) {
+  console.log("andi lulus");
+} else {
+  console.log("andi tidak lulus");
+}
+
+// soal 2
+if (nilaiMTK >= 91 || nilaiIPA >= 91 || nilaiIPS >= 91) {
+  console.log("andi lulus");
+} else {
+  console.log("andi tidak lulus");
+}
+//soal 3
+let isLoggedIn = true
+if (!isLoggedIn) {
+    console.log("anda sudah login");
+    
+} else {
+    console.log("anda belum login");
+    
+}
+
