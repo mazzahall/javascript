b817066dcaaf2b060aba792b939ddee93006adc3
mazzahall <abdullah0210@student.abudzar.sch.id>
2025-05-08 11:30:57 +0700

    tugas-14

---
+++ tugas-14/script.js
@@ -0,0 +1,17 @@
+// soal
+// deklarasi function
+function calculateArea(length, width) {
+    return length * width
+}
+// jawab
+// expression function
+const calculateBrea = function (length, width) {
+    return length * width
+    
+}
+// arrow function
+const calculateCrea = (length , width) => length * width
+
+calculateArea(10, 5)
+calculateBrea(10, 5)
+calculateCrea(10, 5)
